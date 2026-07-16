7d0a334feaec5bbeedda6c817dac3a7904fdd0b4
tanfeng <tanfeng1990@gmail.com>
2026-07-16 09:59:20 +0800

    optimize install.sh

--- glm5.2/install.sh
+++ glm5.2/install.sh
@@ -1,2 +1,6 @@
 cd ../../vllm-ascend
-pip install -v -e . --no-deps
+# 安装整个vllm-ascend
+PIP_INDEX_URL=https://mirrors.huaweicloud.com/repository/pypi/simple \
+PIP_EXTRA_INDEX_URL=https://mirrors.huaweicloud.com/ascend/repos/pypi \
+PIP_TRUSTED_HOST=mirrors.huaweicloud.com \
+pip install -v -e .

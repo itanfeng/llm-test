b722062a86c96ef7da25dd6abb1fcdb6a9283acc
tanfeng <tanfeng1990@gmail.com>
2026-07-16 11:09:20 +0800

    add swe

--- glm5.2/run_data.sh
+++ glm5.2/run_data.sh
@@ -1,1 +1,3 @@
-python sample_data.py --count 10 --seed 42 --output swe_prompts.jsonl
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_HOME=/data/tf/llm-test/data
+python hf_data.py --count 10 --seed 42 --output swe_prompts.jsonl

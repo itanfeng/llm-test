81f23bbff72b37703d1f21896e7a9802c5fd58ef
tanfeng <tanfeng1990@gmail.com>
2026-08-20 14:44:37 +0800

    update install

--- glm5.1/install.sh
+++ glm5.1/install.sh
@@ -1,6 +1,8 @@
+# 保存当前脚本所在目录
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ../../vllm-ascend-v0.18.0
-# 安装整个vllm-ascend
+# 安装整个 vllm-ascend，日志保存到当前脚本目录
 PIP_INDEX_URL=https://mirrors.huaweicloud.com/repository/pypi/simple \
 PIP_EXTRA_INDEX_URL=https://mirrors.huaweicloud.com/ascend/repos/pypi \
 PIP_TRUSTED_HOST=mirrors.huaweicloud.com \
-pip install -v -e . 2>&1 | tee vllm-ascend-v0.18.0-install.log
+pip install -v -e . 2>&1 | tee "${SCRIPT_DIR}/vllm-ascend-v0.18.0-install.log"

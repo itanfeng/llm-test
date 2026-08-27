4661976ab40d7eb5eee463d70f5ae68995342f81
tanfeng <tanfeng1990@gmail.com>
2026-08-27 11:05:25 +0800

    fix

--- glm5.2-a5/install.sh
+++ glm5.2-a5/install.sh
@@ -1,10 +1,11 @@
-Ip_vpn='90.254.59.83'
-export http_proxy="http://p_atlas:proxy%40123@${Ip_vpn}:6688"
-export https_proxy="http://p_atlas:proxy%40123@${Ip_vpn}:6688"
+export http_proxy="http://p_atlas:proxy%40123@90.255.20.111:8888"
+export https_proxy="$http_proxy"
+export NO_PROXY=""  
 
 # 保存当前脚本所在目录
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd ../vllm-ascend
+VLLM_ASCEND_ROOT="${VLLM_ASCEND_ROOT:-${SCRIPT_DIR}/../../vllm-ascend}"
+cd "${VLLM_ASCEND_ROOT}"
 # 安装整个 vllm-ascend，日志保存到当前脚本目录
 PIP_INDEX_URL=https://mirrors.huaweicloud.com/repository/pypi/simple \
 PIP_EXTRA_INDEX_URL=https://mirrors.huaweicloud.com/ascend/repos/pypi \
